862c6b86acad6f2de356ea6b11fe24c318908ac4
Arltzi <arieh@zitron.ca>
2020-10-17 19:15:07 -0700

    quick edit

--- Assets/Scripts/HunterSCollision.cs
+++ Assets/Scripts/HunterSCollision.cs
@@ -12,7 +12,7 @@ public class HunterSCollision : MonoBehaviour
     void OnCollisionEnter(Collision other)
     {
         RaycastHit rch;
-        Physics.Raycast(transform.position, other.transform.position - transform.position, out rch);
+        Physics.Raycast(transform.position, other.gameObject.transform.position - transform.position, out rch);
         Debug.DrawRay(transform.position, other.transform.position - transform.position, Color.yellow, 0.5f, true);
         // FIGURE OUT IF STATEMENT CONDITIONS
         if (rch.transform.gameObject.GetComponent<HealthAndDamage>())
